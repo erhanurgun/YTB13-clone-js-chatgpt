dee9082327120825915a72491d30b0c67516aacf
erhanurgun <erhan.rgn04@gmail.com>
2023-01-01 21:04:02 +0300

    readme.md dosyasi duzenlendi - 1

--- server/server.js
+++ server/server.js
@@ -30,7 +30,7 @@ app.post("/", async (req, res) => {
       model: "code-davinci-002",
       prompt: `${prompt}`,
       temperature: 0,
-      max_tokens: 3000,
+      max_tokens: 100,
       top_p: 1,
       frequency_penalty: 0.5,
       presence_penalty: 0,

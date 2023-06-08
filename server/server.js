ffdfabbd8428154603133467fe7abb27a8790bc6
erhanurgun <erhan.rgn04@gmail.com>
2023-06-08 22:48:50 +0300

    kontrol noktasi olusturuldu - 9

--- server/server.js
+++ server/server.js
@@ -41,7 +41,7 @@ app.post("/", async (req, res) => {
     });
   } catch (error) {
     console.log(error);
-    res.status(500).send({ error });
+    // res.status(500).send({ error });
   }
 });
 
